6b9cd8a430ab07526905cafdc2b33d3947c523df
vitor <vitorekrlol@gmail.com>
2023-05-21 13:07:22 -0300

    Triangulação do caso OU INCLUSIVO

--- src/tst/AtomicFieldTest.java
+++ src/tst/AtomicFieldTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import app.AtomicField;
+import app.CompositeField;
 import app.Field;
 
 public class AtomicFieldTest {
@@ -18,6 +19,23 @@ public class AtomicFieldTest {
     public void emptyAtomicFieldIsNotComplete() {
         Field field = new AtomicField("CPF", "");
         assertFalse(field.isComplete());
+    }
+
+    @Test
+    public void compositeFieldIsComplete() {
+        CompositeField nome = new CompositeField("Nome");
+        nome.addChild(new AtomicField("PrimeiroNome", "João"));
+        nome.addChild(new AtomicField("UltimoNome", "Silva"));
+        assertTrue(nome.isComplete());
+    }
+
+
+    @Test
+    public void incompleteCompositeFieldIsNotComplete() {
+    	CompositeField nome = new CompositeField("Nome");
+        nome.addChild(new AtomicField("PrimeiroNome", "João"));
+        nome.addChild(new AtomicField("UltimoNome", ""));
+        assertFalse(nome.isComplete());
     }
 }
 
